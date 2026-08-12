82b50d880a28d3375e0aa918debb09856e6cd91e
LJJ(YL) <15084854019@163.com>
2026-08-12 14:37:06 +0800

    antd下载配置

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
-
+import { Button } from 'antd';
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home首页</h1>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
 }
