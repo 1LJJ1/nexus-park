99a76711c99a93272f955184ed9f4c11da113908
LJJ(YL) <15084854019@163.com>
2026-08-12 17:32:48 +0800

    登录页面样式基本完成

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import AppRouter from './router';
 import 'modern-normalize';
+import '@/assets/styles/base.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import style from './index.module.scss';
+import logo from '@/assets/images/logo.png';
+import { Button, Form, Input } from 'antd';
 
+type FieldType = {
+  username?: string;
+  password?: string;
+};
+
+function LoginForm() {
+  return (
+    <div className={style['login-form']}>
+      <div className={style.leftbg}></div>
+      <div className={style.part}>
+        <div className={style.title}>
+          <img src={logo} />
+          <h3>智慧园区管理平台</h3>
+        </div>
+        <Form labelCol={{ span: 4 }}>
+          <Form.Item<FieldType> label="用户名" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item<FieldType> label="密码" name="password">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
+            <Button type="primary">登录</Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+}
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className={style['login-container']}>
+      <LoginForm />
+    </div>
+  );
 }
