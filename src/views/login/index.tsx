884fc12e50aacd118312bcd7322c6834c4983baa
LJJ(YL) <15084854019@163.com>
2026-08-13 10:31:53 +0800

    登录表单完整校验，样式调整

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,18 +1,25 @@
 import style from './index.module.scss';
 import logo from '@/assets/images/logo.png';
 import { Button, Form, Input } from 'antd';
-
 const formFields = [
   {
     name: 'username',
     label: '用户名',
-    rules: [{ required: true, message: '请输入用户名' }],
+    rules: [
+      { required: true, message: '请输入用户名' },
+      { max: 6, message: '用户名最长6位' },
+      { min: 3, message: '用户名最短3位' },
+    ],
     component: <Input placeholder="请输入用户名" />,
   },
   {
     name: 'password',
     label: '密码',
-    rules: [{ required: true, message: '请输入密码' }],
+    rules: [
+      { required: true, message: '请输入密码' },
+      { max: 16, message: '密码最长16位' },
+      { min: 6, message: '密码最短6位' },
+    ],
     component: <Input.Password placeholder="请输入密码" />,
   },
 ];
@@ -35,7 +42,7 @@ function LoginForm() {
           <h3>智慧园区管理平台</h3>
         </div>
 
-        <Form labelCol={{ span: 5 }}>
+        <Form form={form} labelCol={{ span: 5 }}>
           {formFields.map((item) => {
             return (
               <Form.Item key={item.name} name={item.name} label={item.label} rules={item.rules}>
@@ -43,7 +50,7 @@ function LoginForm() {
               </Form.Item>
             );
           })}
-          <Form.Item wrapperCol={{ offset: 4, span: 20 }}>
+          <Form.Item wrapperCol={{ offset: 5, span: 20 }}>
             <Button type="primary" block onClick={submit}>
               登录
             </Button>
