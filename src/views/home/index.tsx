8de8a96a7637cc37a99c9f8306d72c7f9f2f15f7
LJJ(YL) <15084854019@163.com>
2026-08-17 15:50:50 +0800

    home页面基本布局

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,7 +1,60 @@
+import React, { useState } from 'react';
+
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+const items: MenuItem[] = [getItem('Option 1', '1'), getItem('Option 2', '2')];
+
 export default function Home() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <div>
-      <h1>Home首页</h1>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div
+          style={{
+            height: '64px',
+            textAlign: 'center',
+            color: '#fff',
+            lineHeight: '64px',
+            fontSize: '20px',
+            fontWeight: 'bold',
+          }}
+        >
+          智慧中台
+        </div>
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }} items={[{ title: 'User' }, { title: 'Bill' }]} />
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          ></div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>尾部</Footer>
+      </Layout>
+    </Layout>
   );
 }
