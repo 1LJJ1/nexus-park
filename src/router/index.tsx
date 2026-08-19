7a4c85f395d4ebd4c2a72f4a285a62a8ebb50df6
LJJ(YL) <15084854019@163.com>
2026-08-19 16:41:25 +0800

    home修改layout，删除动态路由，重新编写

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,36 +1,31 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
-import { generateRoutes } from './generateRoutes';
+
 import { useSelector } from 'react-redux';
 import RequireAuth from '@/components/RequireAuth';
 import type { RootState } from '@/store';
-const Home = lazy(() => import('@/views/home/index.tsx'));
+const Layout = lazy(() => import('@/views/layout/index.tsx'));
 const Login = lazy(() => import('@/views/login/index'));
 const NoFound = lazy(() => import('@/views/notFound/index'));
 const AppRouter = () => {
   const menuList = useSelector((state: RootState) => state.menuReducer.menu);
-  const router = generateRoutes(menuList);
-  console.log(router);
+  console.log(menuList);
   return (
     <Suspense fallback={<div>加载中</div>}>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/" element={<Navigate to="/layout" replace />} />
         <Route
-          path="home"
+          path="layout"
           element={
             <RequireAuth allowed redirectTo="/login">
-              <Home />
+              <Layout />
             </RequireAuth>
           }
-        >
-          {router.map((item) => (
-            <Route key={item.path} path={item.path} element={item.element} />
-          ))}
-        </Route>
+        ></Route>
         <Route
           path="login"
           element={
-            <RequireAuth allowed={false} redirectTo="/home">
+            <RequireAuth allowed={false} redirectTo="/layout">
               <Login />
             </RequireAuth>
           }
