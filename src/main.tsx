70efbd2e7dd3d1cd5cbc511e809be9d9b7d59e00
LJJ(YL) <15084854019@163.com>
2026-08-24 17:47:46 +0800

    antd国际化处理

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ConfigProvider } from 'antd';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
@@ -5,10 +6,17 @@ import AppRouter from './router';
 import { store } from '@/store/index';
 import 'modern-normalize/modern-normalize.css';
 import '@/assets/styles/base.css';
+import zhCN from 'antd/locale/zh_CN';
+// import en_GB from 'antd/locale/en_GB';
+// for date-picker i18n
+import 'dayjs/locale/zh-cn';
+
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <Provider store={store}>
-      <AppRouter />
-    </Provider>
+    <ConfigProvider locale={zhCN}>
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
+    </ConfigProvider>
   </BrowserRouter>
 );
