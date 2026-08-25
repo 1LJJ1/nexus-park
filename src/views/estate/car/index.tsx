13d249839e309bbd3a328a7cb1a61bd006bdd1b3
LJJ(YL) <15084854019@163.com>
2026-08-25 17:50:09 +0800

    车辆管理基本完成

--- src/views/estate/car/index.tsx
+++ src/views/estate/car/index.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
-
+import { Card, Row, Input, Button } from 'antd';
+import { useState } from 'react';
 export default function Car() {
-  return <div>车辆管理</div>;
+  const [keyword, setKeyword] = useState<string>('');
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setKeyword(e.target.value);
+  };
+  return (
+    <>
+      <Row>
+        <Card style={{ width: '100%' }}>
+          <div style={{ width: '300px', display: 'flex', alignItems: 'center' }}>
+            <Input
+              value={keyword}
+              placeholder="输入车牌号，手机或者联系人"
+              allowClear
+              onChange={handleChange}
+            ></Input>
+            <Button style={{ marginLeft: '10px' }} type="primary">
+              查询
+            </Button>
+          </div>
+        </Card>
+      </Row>
+      <Row style={{ marginTop: '20px' }}>
+        <Card>表格(略)</Card>
+      </Row>
+    </>
+  );
 }
